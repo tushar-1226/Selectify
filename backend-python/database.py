46d2fd6b6d883dbd3ad924c776eca3387ab23e43
tushar-1226 <170326035+tushar-1226@users.noreply.github.com>
2026-03-07 12:08:44 +0530

    fix(backend): Emancipate from libpq by using pg8000 pure Python Postgres driver

--- backend-python/database.py
+++ backend-python/database.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./selectify.db")
 if DATABASE_URL and DATABASE_URL.startswith("postgres"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+pg8000://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+pg8000://", 1)
 
 # For SQLite, we need connect_args to allow multi-threaded access
 if DATABASE_URL.startswith("sqlite"):
